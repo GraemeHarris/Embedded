e5591571d8b6767953bb37e26777ed7948897e6f
RobustBiscuit <irshaaddodia@gmail,com>
2018-09-05 22:01:56 +0200

    Added MCP3008 Adafruit (Python 3) library. Implemented boiler plate code.

--- PythonCodePrac04.py
+++ PythonCodePrac04.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
 #DDXIRS001 - Irshaad Dodia
-#
+#HRRGRA004 - Graeme Harris
 #EEE3096S - Embedded Systems II
 #Prac 04 - SPI and Interrupts
 
-
-
-#!/usr/bin/python
 import RPi.GPIO as GPIO
+import Adafruit_MCP3008
+import time
+import os
+
 
 GPIO.setmode(GPIO.BCM)    #Pin Numbering
 
@@ -16,6 +18,16 @@ Sw2 = 24 #Blue
 Sw3 = 25 #Green
 Sw4 = 17 #Yellow
 
+SPICLK = 11 #Purple
+SPIMISO = 9 #Blue
+SPIMOSI = 10 #Green
+SPICS = 8 #Yellow
+
+#Note:
+#White = 3v3
+#Red = 5v
+#Black = GND
+
 
 #Setting GPIO I/O 
 GPIO.setup(Sw1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -23,8 +35,22 @@ GPIO.setup(Sw2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(Sw3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(Sw4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
 
+mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
+# global variable
+values = [0]*8
+
+while True:
+    for i in range(8):
+        values[i] = mcp.read_adc(i)
+        # delay for a half second
+        time.sleep(0.1)
+    print(values)
 
 def reset_cb(channel): #When Switch 1 is triggered
         
